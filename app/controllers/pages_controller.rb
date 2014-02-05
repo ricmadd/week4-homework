45f6e7ec6c3114de505f753e7e6247d72791594e
RicardoMaduro <rmaduro@gmail.com>
2014-02-05 01:01:54 -0600

    week4-homework
    -Rebuilt weather app from class.
    -Built "index" page with one list
    - Built 3 "show" pages with lists within "index" lists.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,21 @@
-  class PagesController < ApplicationController
-  end
+require 'open-uri'
+
+class PagesController < ApplicationController
+
+    def weathersearch
+    end
+
+    def weatherconditions
+          @city = params[:city]
+          url = URI.escape("http://api.openweathermap.org/data/2.5/weather?q=#{@city}&units=imperial")
+          @json_data = open(url).read
+          @data = JSON.parse(@json_data)
+    end
+
+    def index
+    end
+
+    def show
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Week4hw::Application.routes.draw do
 
-  get "/" => "pages#home"
-
   # HINT: Add more routes here...
 
+  get "/" => "pages#home"
 
+  get "/weather/search" => "pages#weathersearch"
+
+  get "/weather/conditions" => "pages#weatherconditions"
+
+  get "/index" => "pages#index"
+
+  get "/show" => "pages#showWBS_1_1_0"
+
+  get "/showWBS_1_1_0" => "pages#showWBS_1_1_0"
+
+  get "/showWBS_1_2_0" => "pages#showWBS_1_2_0"
+
+  get "/showWBS_1_3_0" => "pages#showWBS_1_3_0"
 
 end
